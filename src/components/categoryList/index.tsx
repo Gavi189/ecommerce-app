8322c7a41773676f2692ef2e07145c8659df8ed2
Seu Nome <seuemail@exemplo.com>
2025-08-22 14:48:26 -0300

    "Component FlatList"

--- src/components/categoryList/index.tsx
+++ src/components/categoryList/index.tsx
@@ -1,24 +1,26 @@
 import { View, Text, FlatList } from "react-native";
 import CategoryItem from "../categoryItem";
 
+//Dados simulados das categorias "data.ts ou data.json"
 const categories = [
   { id: "1", name: "Eletrônicos" },
   { id: "2", name: "Roupas" },
   { id: "3", name: "Esportes" },
+  { id: "4", name: "Livros" },
+  { id: "5", name: "Beleza" },
+  { id: "6", name: "Casa" },
 ];
 
 export default function CategoryList() {
   return (
-    <View>
-      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 8 }}>
-        Categorias
-      </Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <FlatList
-        data={categories}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <CategoryItem name={item.name} />}
+        data={categories} //Onde os dados são passados como array na props data
+        horizontal //Para deixar a lista posição horizontal
+        showsHorizontalScrollIndicator={false} //Para esconder a barra de rolagem horizontal
+        keyExtractor={(item) => item.id} //Para definir uma chave única para cada item
+        renderItem={({ item }) => <CategoryItem name={item.name} />} //Para renderizar cada item da lista,
+        // passando o nome da categoria como prop para o componente CategoryItem
       />
     </View>
   );
--- src/components/promo/style.ts
+++ src/components/promo/style.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: 375,
+    marginBottom: 16,
   },
   image: {
     width: 350,
